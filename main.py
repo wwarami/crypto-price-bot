6d9a9b669174934917b18d08527d53fb897e5492
wwaramii <wwaramii@gmail.com>
2024-02-11 16:20:37 +0330

    .env file added😑

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import os
 import asyncio
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from crypto_track.database.run import init_db
+
+# Load .env variables
+load_dotenv()
+
 
 async def main():
-    ...
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    DB_ENGINE = await init_db(database_url=DATABASE_URL)
+    async_session = async_sessionmaker(DB_ENGINE, expire_on_commit=False)
 
 
 if __name__ == "__main__":
