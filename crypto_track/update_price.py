5ec077c25e697b0f3197eeff078456e7821c6bbf
Aram Khezri <141460301+wwarami@users.noreply.github.com>
2024-03-02 15:59:42 +0000

    ✅ Routine price update implemented

--- crypto_track/update_price.py
+++ crypto_track/update_price.py
@@ -1,10 +1,11 @@
 from crypto_track.database.manager import AsyncDatabaseManager
 from crypto_track.api.manager import AsyncAPIManager
 
+async def update_prices_manager():
+    await update_all_cryptos()
 
 async def update_all_cryptos():
     available_cryptos = await AsyncDatabaseManager().get_all_cryptos()
-    print(available_cryptos)
     cryptos_symbols_list = [crypto.symbol for crypto in available_cryptos]
     try:
         update_raw_data = await AsyncAPIManager().get_crpyto_prices(cryptos_symbols_list)
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import asyncio
 import logging
 import sys
+import threading
+import time
+import asyncio
+import schedule
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from crypto_track.database.run import init_db
@@ -12,7 +16,7 @@ from aiogram import Bot, Dispatcher
 from crypto_track.signup import signup_router
 from crypto_track.profile import profile_router
 from crypto_track.crypto_price import crypto_price_router
-from crypto_track.update_price import update_all_cryptos
+from crypto_track.update_price import update_prices_manager
 
 
 dp = Dispatcher()
@@ -21,6 +25,24 @@ load_dotenv()
 # Aiogram logging
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
+
+def run_continuously(interval=1):
+    cease_continuous_run = threading.Event()
+
+    class ScheduleThread(threading.Thread):
+        @classmethod
+        def run(cls):
+            while not cease_continuous_run.is_set():
+                schedule.run_pending()
+                time.sleep(interval)
+
+    continuous_thread = ScheduleThread()
+    continuous_thread.start()
+    return cease_continuous_run
+
+
+def run_prices_updater():
+    asyncio.run(update_prices_manager())
 
 async def main():
     DATABASE_URL = os.getenv('DATABASE_URL')
@@ -32,11 +54,15 @@ async def main():
     database_manager_instance = AsyncDatabaseManager(async_session=async_session)
     api_manager_instance = AsyncAPIManager(api_key=API_KEY)
 
+    schedule.every(5).minutes.do(run_prices_updater)
+    stop_run_continuously = run_continuously()
+
     bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp.include_router(signup_router)
     dp.include_router(profile_router)
     dp.include_router(crypto_price_router)
     await dp.start_polling(bot)
+    stop_run_continuously.set()
 
 if __name__ == "__main__":
     asyncio.run(main()) 
